340115ff3a26a287e6ef34aeefbef2221576bd40
eliyaz-kl <55746134+eliyaz-kl@users.noreply.github.com>
2019-10-09 13:28:24 +0300

    prediction code

---
+++ predict.py
@@ -0,0 +1,248 @@
+# -*- coding: utf-8 -*-
+
+import os
+import itertools
+import codecs
+import re
+import datetime
+import cairocffi as cairo
+import editdistance
+import numpy as np
+from scipy import ndimage
+import pylab
+
+from keras import backend as K
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers import Input, Dense, Activation
+from keras.layers import Reshape, Lambda
+from keras.layers.merge import add, concatenate
+from keras.models import Model
+from keras.layers.recurrent import GRU
+from keras.optimizers import SGD
+from keras.utils.data_utils import get_file
+from keras.preprocessing import image
+import keras.callbacks
+from keras.models import load_model
+from keras.preprocessing.image import load_img,img_to_array
+
+import matplotlib.pyplot as plt
+
+def ctc_lambda_func(args):
+    y_pred, labels, input_length, label_length = args
+    # the 2 is critical here since the first couple outputs of the RNN
+    # tend to be garbage:
+    y_pred = y_pred[:, 2:, :]
+    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
+
+# Reverse translation of numerical classes back to characters
+def labels_to_text(labels):
+    ret = []
+    for c in labels:
+        if c == len(alphabet):  # CTC Blank
+            ret.append("")
+        else:
+            ret.append(alphabet[c])
+    return "".join(ret)
+
+alphabet = u'abcdefghijklmnopqrstuvwxyz '
+
+absolute_max_string_len = 16
+character_count = 28
+
+# weight_file = './weights24.h5'
+weight_file = './weights24.h5'
+
+img_w = 128
+# Input Parameters
+img_h = 64
+words_per_epoch = 16000
+val_split = 0.2
+val_words = int(words_per_epoch * (val_split))
+
+# Network parameters
+conv_filters = 16
+kernel_size = (3, 3)
+pool_size = 2
+time_dense_size = 32
+rnn_size = 512
+minibatch_size = 32
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (1, img_w, img_h)
+else:
+    input_shape = (img_w, img_h, 1)
+
+
+act = 'relu'
+input_data = Input(name='the_input', shape=input_shape, dtype='float32')
+inner = Conv2D(conv_filters, kernel_size, padding='same',
+               activation=act, kernel_initializer='he_normal',
+               name='conv1')(input_data)
+inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max1')(inner)
+inner = Conv2D(conv_filters, kernel_size, padding='same',
+               activation=act, kernel_initializer='he_normal',
+               name='conv2')(inner)
+inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max2')(inner)
+
+conv_to_rnn_dims = (img_w // (pool_size ** 2), (img_h // (pool_size ** 2)) * conv_filters)
+inner = Reshape(target_shape=conv_to_rnn_dims, name='reshape')(inner)
+
+# cuts down input size going into RNN:
+inner = Dense(time_dense_size, activation=act, name='dense1')(inner)
+
+# Two layers of bidirectional GRUs
+# GRU seems to work as well, if not better than LSTM:
+gru_1 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru1')(inner)
+gru_1b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru1_b')(inner)
+gru1_merged = add([gru_1, gru_1b])
+gru_2 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru2')(gru1_merged)
+gru_2b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru2_b')(gru1_merged)
+
+# transforms RNN output to character activations:
+inner = Dense(character_count, kernel_initializer='he_normal',
+              name='dense2')(concatenate([gru_2, gru_2b]))
+y_pred = Activation('softmax', name='softmax')(inner)
+Model(inputs=input_data, outputs=y_pred).summary()
+
+labels = Input(name='the_labels', shape=[absolute_max_string_len], dtype='float32')
+input_length = Input(name='input_length', shape=[1], dtype='int64')
+label_length = Input(name='label_length', shape=[1], dtype='int64')
+# Keras doesn't currently support loss funcs with extra parameters
+# so CTC loss is implemented in a lambda layer
+loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
+
+# clipnorm seems to speeds up convergence
+sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
+
+model = Model(inputs=[input_data, labels, input_length, label_length], outputs=loss_out)
+
+# the loss calc occurs elsewhere, so use a dummy lambda func for the loss
+model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd)
+model.load_weights(weight_file)
+# captures output of softmax so we can decode the output during visualization
+#test_func = K.function([input_data], [y_pred])
+
+
+model_p = Model(inputs=input_data, outputs=y_pred)
+
+#model_p = load_model(MODEL_PATH)
+
+def decode_predict_ctc(out, top_paths = 1):
+    results = []
+    beam_width = 5
+    if beam_width < top_paths:
+      beam_width = top_paths
+    for i in range(top_paths):
+      lables = K.get_value(K.ctc_decode(out, input_length=np.ones(out.shape[0])*out.shape[1],
+                           greedy=False, beam_width=beam_width, top_paths=top_paths)[0][i])[0]
+      text = labels_to_text(lables)
+      results.append(text)
+    return results
+  
+def predit_a_image(a, top_paths = 1):
+  c = np.expand_dims(a.T, axis=0)
+  net_out_value = model_p.predict(c)
+  top_pred_texts = decode_predict_ctc(net_out_value, top_paths)
+  return top_pred_texts
+
+def paint_text(text, w, h, rotate=False, ud=False, multi_fonts=False):
+    surface = cairo.ImageSurface(cairo.FORMAT_RGB24, w, h)
+    with cairo.Context(surface) as context:
+        context.set_source_rgb(1, 1, 1)  # White
+        context.paint()
+        # this font list works in CentOS 7
+        if multi_fonts:
+            fonts = [
+                'Century Schoolbook', 'Courier', 'STIX',
+                'URW Chancery L', 'FreeMono']
+            context.select_font_face(
+                np.random.choice(fonts),
+                cairo.FONT_SLANT_NORMAL,
+                np.random.choice([cairo.FONT_WEIGHT_BOLD, cairo.FONT_WEIGHT_NORMAL]))
+        else:
+            context.select_font_face('Courier',
+                                     cairo.FONT_SLANT_NORMAL,
+                                     cairo.FONT_WEIGHT_BOLD)
+        context.set_font_size(25)
+        box = context.text_extents(text)
+        border_w_h = (4, 4)
+        if box[2] > (w - 2 * border_w_h[1]) or box[3] > (h - 2 * border_w_h[0]):
+            raise IOError(('Could not fit string into image.'
+                           'Max char count is too large for given image width.'))
+
+        # teach the RNN translational invariance by
+        # fitting text box randomly on canvas, with some room to rotate
+        max_shift_x = w - box[2] - border_w_h[0]
+        max_shift_y = h - box[3] - border_w_h[1]
+        top_left_x = np.random.randint(0, int(max_shift_x))
+        if ud:
+            top_left_y = np.random.randint(0, int(max_shift_y))
+        else:
+            top_left_y = h // 2
+        context.move_to(top_left_x - int(box[0]), top_left_y - int(box[1]))
+        context.set_source_rgb(0, 0, 0)
+        context.show_text(text)
+
+    buf = surface.get_data()
+    a = np.frombuffer(buf, np.uint8)
+    a.shape = (h, w, 4)
+    a = a[:, :, 0]  # grab single channel
+    a = a.astype(np.float32) / 255
+    a = np.expand_dims(a, 0)
+    if rotate:
+        a = image.random_rotation(a, 3 * (w - top_left_x) / w + 1)
+    #a = speckle(a)
+
+    return a
+
+h = 64
+w = 128
+a = paint_text('a game',h = h, w = w)
+b = a.reshape((h, w))
+#plt.imshow(b, cmap='Greys_r')
+#plt.show()
+print(a.shape)
+c = np.expand_dims(a.T, axis=0)
+#print(c)
+
+net_out_value = model_p.predict(c)
+pred_texts = decode_predict_ctc(net_out_value)
+print(pred_texts)
+
+
+img = load_img("test3.png", target_size=(h,w))
+
+#plt.imshow(img, cmap='Greys_r')
+#plt.show()
+c = img_to_array(img) 
+print(c.shape)
+c = c / 255.0
+c = c[:, :, 0]  # grab single channel
+c = np.expand_dims(c, 0)
+print(c.shape)
+c = np.expand_dims(c.T, axis=0)
+#print(c)
+
+net_out_value = model_p.predict(c)
+pred_texts = decode_predict_ctc(net_out_value)
+print(pred_texts)
+
+
+#predit_a_image(a, top_paths = 3)
+
+#plt.imshow(net_out_value[0].T, cmap='binary', interpolation='nearest')
+#plt.show()
+
+#K.get_value(K.ctc_decode(net_out_value, input_length=np.ones(net_out_value.shape[0])*net_out_value.shape[1],
+#                         greedy=False, beam_width=3, top_paths=3)[0][0])
+
+
+#K.ctc_decode(net_out_value, input_length=np.ones(net_out_value.shape[0])*net_out_value.shape[1],
+#                         greedy=False, beam_width=5, top_paths=3)
+
+
+#K.get_value(K.ctc_decode(net_out_value, input_length=np.ones(net_out_value.shape[0])*net_out_value.shape[1],
+#                         greedy=False, beam_width=3, top_paths=3)[0][0])
+
+
+
